97e557369a3b810023378510f5bbba1658b2306a
kishore-a <csuftitan@LTMacAir-M1-165017.local>
2022-12-15 20:43:28 -0800

    made changes to database file

--- app/src/main/java/com/example/finalproject/Sqlite.kt
+++ app/src/main/java/com/example/finalproject/Sqlite.kt
@@ -48,45 +48,34 @@ class Sqlite : AppCompatActivity() {
             // name to our database
             db.addName(name, age)
 
-            // Toast to message on the screen
             Toast.makeText(this, name + " added to database", Toast.LENGTH_LONG).show()
 
-            // at last, clearing edit texts
             enterName.text.clear()
             enterAge.text.clear()
         }
 
-        // below code is to add on  click
-        // listener to our print name button
+       
         printName.setOnClickListener{
 
-            // creating a DBHelper class
-            // and passing context to it
+     
             val db = DBHelper(this, null)
 
-            // below is the variable for cursor
-            // we have called method to get
-            // all names from our database
-            // and add to name text view
+         
             val cursor = db.getName()
 
-            // moving the cursor to first position and
-            // appending value in the text view
+            
 
             cursor!!.moveToFirst()
 
                 Name.append(cursor.getString(cursor.getColumnIndex(DBHelper.NAME_COl)) + "\n")
                 Age.append(cursor.getString(cursor.getColumnIndex(DBHelper.AGE_COL)) + "\n")
 
-                // moving our cursor to next
-                // position and appending values
                 while(cursor.moveToNext()){
                     Name.append(cursor.getString(cursor.getColumnIndex(DBHelper.NAME_COl)) + "\n")
                     Age.append(cursor.getString(cursor.getColumnIndex(DBHelper.AGE_COL)) + "\n")
                 }
 
 
-            // at last we close our cursor
             cursor.close()
         }
     }
